7ffb1c8b9a56084341c1311bad517187d0cff7ba
mvimercati <mvimercati@gmail.com>
2017-10-24 21:28:21 +0100

    Customizable values roundment.

--- vitalk-cayenne.js
+++ vitalk-cayenne.js
@@ -12,34 +12,34 @@ var queue = [];
 
 
 var cmds = {
-    "OutdoorTemp"         : [null,  60, null,  1, "temp", "c",       '5525', 2, 10],
-    "SolarPanelTemp"      : [null,  60, null,  2, "temp", "c",       '6564', 2, 10],
-    "HotWaterTemp"        : [null,  30, null,  3, "temp", "c",       '0804', 2, 10],
-    "HotWaterTempTarget"  : [null,  30, null,  4, "temp", "c",       '6300', 1, 1],
-    "BurnerTemp"          : [null,   5, null,  6, "temp", "c",       '0802', 2, 10],
-    "HeatingTempTarget"   : [null,  60, null,  7, "temp", "c",       '555A', 2, 10],
-    "ExhaustGasTemp"      : [null,  30, null,  9, "temp", "c",       '0808', 2, 10],
-    "StartsCounterBurner" : [null,  60, null, 10, "digital", "null", '088A', 2, 1],
-    "RuntimeHoursBurner"  : [null, 600, null, 11, "digital", "null", '08A7', 4, 3600],
-    "BurnerPowerThrottle" : [null,   5, null, 12, "analog", "p",     'A38F', 1, 2],
-    "BoilerLowerTemp"     : [null,  30, null, 15, "temp", "c",       '6566', 2, 10],
-    "BoilerLoading"       : [null,   5, null, 17, "digital", "null", '6513', 1, 1],
-    "SolarPumpActive"     : [null,  30, null, 18, "digital", "null", '6552', 1, 1],
-    "InternalPump"        : [null,   5, null, 19, "analog", "p",     '0A3C', 1, 1],
-    "HeatingRequest"      : [null,  60, null, 20, "digital", "d",    '0A80', 1, 1],
-    "RuntimeHoursSolar"   : [null, 900, null, 21, "digital", "null", '6568', 2, 1],
-    "TotalSolarEnergy"    : [null, 120, null, 22, "digital", "null", '6560', 4, 1], /* kWh */
-    "SwitchingValvePos"   : [null,  15, null, 23, "digital", "null", '0A10', 1, 1],
+    "OutdoorTemp"         : [null,  60, null,  1, "temp", "c",       '5525', 2, 10, 10],
+    "SolarPanelTemp"      : [null,  60, null,  2, "temp", "c",       '6564', 2, 10, 10],
+    "HotWaterTemp"        : [null,  30, null,  3, "temp", "c",       '0804', 2, 10, 10],
+    "HotWaterTempTarget"  : [null,  30, null,  4, "temp", "c",       '6300', 1, 1,   1],
+    "BurnerTemp"          : [null,   5, null,  6, "temp", "c",       '0802', 2, 10,  1],
+    "HeatingTempTarget"   : [null,  60, null,  7, "temp", "c",       '555A', 2, 10,  1],
+    "ExhaustGasTemp"      : [null,  30, null,  9, "temp", "c",       '0808', 2, 10,  1],
+    "StartsCounterBurner" : [null,  60, null, 10, "digital", "null", '088A', 2, 1,   1],
+    "RuntimeHoursBurner"  : [null, 600, null, 11, "digital", "null", '08A7', 4, 3600, 1],
+    "BurnerPowerThrottle" : [null,   5, null, 12, "analog", "p",     'A38F', 1, 2,    1],
+    "BoilerLowerTemp"     : [null,  30, null, 15, "temp", "c",       '6566', 2, 10,  10],
+    "BoilerLoading"       : [null,   5, null, 17, "digital", "null", '6513', 1, 1,    1],
+    "SolarPumpActive"     : [null,  30, null, 18, "digital", "null", '6552', 1, 1,    1],
+    "InternalPump"        : [null,   5, null, 19, "analog", "p",     '0A3C', 1, 1,    1],
+    "HeatingRequest"      : [null,  60, null, 20, "digital", "d",    '0A80', 1, 1,    1],
+    "RuntimeHoursSolar"   : [null, 900, null, 21, "digital", "null", '6568', 2, 1,    1],
+    "TotalSolarEnergy"    : [null, 120, null, 22, "digital", "null", '6560', 4, 1,    1], /* kWh */
+    "SwitchingValvePos"   : [null,  15, null, 23, "digital", "null", '0A10', 1, 1,    1],
 
 /*    "MandataFlowTemp?"    : [null,  60, null, 24, "temp", "c",       '080C', 2, 10],*/
 /*    "ReturnTemp?"         : [null,  60, null, 25, "temp", "c",       '080A', 2, 10],*/
 /*    "WaterFlow?"          : [null,  60, null, 26, "", "",            '0C24', 2, 1],*/
-    "RoomTemp"            : [null,  60, null, 27, "", "",            '2306', 1, 1],
+    "RoomTemp"            : [null,  60, null, 32, "temp", "c",       '2306', 1, 1,    1],
 /*    "StatoPompaRisc?"     : [null,  60, null, 28, "", "",            '7663', 2, 256],*/
 
-    "EnableThermostat"    : [null,  60, null, 29, "", "",            '773A', 1, 1],
-    "StartsCounterSolar"  : [null, 120, null, 30, "", "",            'CF50', 4, 1],
-    "DailySolarEnergy"    : [null, 120, null, 31, "", "",            'CF30', 4, 1], /* Wh */
+    "EnableThermostat"    : [null,  60, null, 29, "", "",            '773A', 1, 1,    1],
+    "StartsCounterSolar"  : [null, 120, null, 30, "", "",            'CF50', 4, 1,    1],
+    "DailySolarEnergy"    : [null, 120, null, 31, "", "",            'CF30', 4, 1000, 10], /* Wh */
 
 /*    "DailySolarEnergyArray0"    : [null, 5, null, 32, "", "",            'CF30', 32, 1],*/
     
@@ -200,8 +200,8 @@ setInterval(function() {
 
 function update(key, value)
 {
-
-    value = Math.round( value / cmds[key][8] );
+    value = value / cmds[key][8];
+    value = Math.round( value * cmds[key][9] ) / cmds[key][9];
 
     if (cmds[key][2] != value) {
 
